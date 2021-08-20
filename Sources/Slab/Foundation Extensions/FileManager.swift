b62e138f46cbe51a5ab705ec8488cd3e72f93f3b
Cyrille Legrand <c.legrand@useradgents.com>
2021-08-20 14:57:52 +0200

    FileManager.allocatedSizeOfDirectory()

--- Sources/Slab/Foundation Extensions/FileManager.swift
+++ Sources/Slab/Foundation Extensions/FileManager.swift
@@ -22,4 +22,52 @@ extension FileManager {
     public var documentsDirectory: URL {
         urls(for: .documentDirectory, in: .userDomainMask).first!
     }
+    
+    public func allocatedSizeOfDirectory(at directoryURL: URL) throws -> UInt64 {
+        var enumeratorError: Error? = nil
+        func errorHandler(_: URL, error: Error) -> Bool {
+            enumeratorError = error
+            return false
+        }
+        
+        let enumerator = self.enumerator(at: directoryURL,
+                                         includingPropertiesForKeys: Array(allocatedSizeResourceKeys),
+                                         options: [],
+                                         errorHandler: errorHandler)!
+        
+        var accumulatedSize: UInt64 = 0
+        for item in enumerator {
+            if enumeratorError != nil { break }
+            let contentItemURL = item as! URL
+            accumulatedSize += try contentItemURL.regularFileAllocatedSize()
+        }
+        if let error = enumeratorError { throw error }
+        return accumulatedSize
+    }
+}
+
+fileprivate let allocatedSizeResourceKeys: Set<URLResourceKey> = [
+    .isRegularFileKey,
+    .fileAllocatedSizeKey,
+    .totalFileAllocatedSizeKey,
+]
+
+
+fileprivate extension URL {
+    
+    func regularFileAllocatedSize() throws -> UInt64 {
+        let resourceValues = try self.resourceValues(forKeys: allocatedSizeResourceKeys)
+        
+        // We only look at regular files.
+        guard resourceValues.isRegularFile ?? false else {
+            return 0
+        }
+        
+        // To get the file's size we first try the most comprehensive value in terms of what
+        // the file may use on disk. This includes metadata, compression (on file system
+        // level) and block size.
+        // In case totalFileAllocatedSize is unavailable we use the fallback value (excluding
+        // meta data and compression) This value should always be available.
+        return UInt64(resourceValues.totalFileAllocatedSize ?? resourceValues.fileAllocatedSize ?? 0)
+    }
 }
