25d766a2954f6caf2208f3ccb61231739250d28d
Audinet <g.audinet@useradgents.com>
2021-10-26 19:33:58 +0200

    Add function to chunk array in array on n elements

--- Sources/Slab/Foundation Extensions/Array.swift
+++ Sources/Slab/Foundation Extensions/Array.swift
@@ -24,6 +24,13 @@ extension Array {
     public mutating func popLast() -> Element? {
         isEmpty ? nil : removeLast()
     }
+    
+    /// Return an array of array of size elements.
+    public func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
 }
 
 extension Array where Element: Equatable {
