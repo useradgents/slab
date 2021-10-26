e9a53e602f30ebe93e3c6f890b4d12d87e60a289
Audinet <g.audinet@useradgents.com>
2021-10-26 11:32:44 +0200

    Add util to display curl string

---
+++ Sources/Slab/Foundation Extensions/URLRequest.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  
+//
+//  Created by Guillaume Audinet on 26/10/2021.
+//
+
+import Foundation
+
+extension URLRequest {
+    /// Small aggregator to have a curl string
+    public var curlString: String {
+        var result = "curl -k "
+        
+        if let method = httpMethod {
+            result += "-X \(method) \\\n"
+        }
+        
+        if let headers = allHTTPHeaderFields {
+            for (header, value) in headers {
+                result += "-H \"\(header): \(value)\" \\\n"
+            }
+        }
+        
+        if let body = httpBody, !body.isEmpty, let string = String(data: body, encoding: .utf8), !string.isEmpty {
+            result += "-d '\(string)' \\\n"
+        }
+        
+        if let url = url {
+            result += url.absoluteString
+        }
+        
+        return result
+    }
+}
