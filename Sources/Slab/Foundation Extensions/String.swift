113cc8027c91e41c37980ea5e8e87c9ac0c76500
Cyrille Legrand <c.legrand@useradgents.com>
2021-11-15 09:32:08 +0100

    Double-cross operator for UPPERCASED translated strings

--- Sources/Slab/Foundation Extensions/String.swift
+++ Sources/Slab/Foundation Extensions/String.swift
@@ -1,14 +1,23 @@
 import Foundation
 import CommonCrypto
 
-// Easy NSLocalizedString operator
+// Easy NSLocalizedString operators
 // "hello"† == NSLocalizedString("hello", comment: "?⃤ hello ?⃤")
+// "hello"‡ == NSLocalizedString("hello", comment: "?⃤ hello ?⃤").localizedUppercase
+
 // Cross † is done as alt+T on a US (QWERTY) or FR (AZERTY) keyboard
 // Looks like a T, reads like Translated
+// Double-cross ‡ is done as shift+alt+7 on a US (QWERTY), or alt+Q on a FR (AZERTY) keyboard.
 postfix operator †
 public postfix func † (left: String) -> String {
     NSLocalizedString(left, comment: "?⃤ " + left + " ?⃤")
 }
+
+postfix operator ‡
+public postfix func ‡ (left: String) -> String {
+    NSLocalizedString(left, comment: "?⃤ " + left + " ?⃤").localizedUppercase
+}
+
 
 extension String {
     /// Tests if the string matches a regular expression
