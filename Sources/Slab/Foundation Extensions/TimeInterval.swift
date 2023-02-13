7c015ff7cec43c49436590c9b7390877bbeaa5a9
g.audinet <g.audinet@useradgents.com>
2023-02-13 17:33:18 +0100

    Add some utils into TimeInterval

--- Sources/Slab/Foundation Extensions/TimeInterval.swift
+++ Sources/Slab/Foundation Extensions/TimeInterval.swift
@@ -11,3 +11,26 @@ extension TimeInterval {
         Int(self / 3600)
     }
 }
+
+extension TimeInterval {
+    // MARK: - Computed Type Properties
+    internal static var secondsPerDay: Double { return 24 * 60 * 60 }
+    internal static var secondsPerHour: Double { return 60 * 60 }
+    internal static var secondsPerMinute: Double { return 60 }
+
+    // MARK: - Type Methods
+    /// - Returns: The time in days using the `TimeInterval` type.
+    public static func days(_ value: Double) -> TimeInterval {
+        return value * secondsPerDay
+    }
+
+    /// - Returns: The time in hours using the `TimeInterval` type.
+    public static func hours(_ value: Double) -> TimeInterval {
+        return value * secondsPerHour
+    }
+
+    /// - Returns: The time in minutes using the `TimeInterval` type.
+    public static func minutes(_ value: Double) -> TimeInterval {
+        return value * secondsPerMinute
+    }
+}
