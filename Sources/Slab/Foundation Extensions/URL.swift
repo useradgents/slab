834b800d451c46d8ba926dfab0763783b3572716
Cyrille Legrand <c.legrand@useradgents.com>
2023-05-25 11:02:20 +0200

    Remove URL extension that crashes Franprix (probably because of swizzling in external libraries)

--- Sources/Slab/Foundation Extensions/URL.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-extension URL: ExpressibleByStringLiteral {
-    /// Creates an URL with any string literal
-    public init(stringLiteral value: String) {
-        self = URL(string: value)!
-    }
-}
