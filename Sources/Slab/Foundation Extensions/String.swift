dae89f1639f2a24c48b3b7e4302626a10dca983e
Cyrille Legrand <c.legrand@useradgents.com>
2023-07-11 14:34:29 +0200

    Add String.levenshteinDistance

--- Sources/Slab/Foundation Extensions/String.swift
+++ Sources/Slab/Foundation Extensions/String.swift
@@ -112,3 +112,62 @@ extension Collection where Element == String? {
         return mapped.isEmpty ? nil : mapped.joined(separator: separator)
     }
 }
+
+
+extension String {
+    public func levenshteinDistance(to other: String) -> Int {
+        let m = self.count
+        let n = other.count
+        
+        var D = _LevenshteinMatrix(m: m, n: n)
+        
+        D[0, 0] = 0
+        
+        for i in 1..<m { D[i, 0] = i }
+        for j in 1..<n { D[0, j] = j }
+        
+        for i in 1..<m {
+            for j in 1..<n {
+                D[i, j] = D.compute(at: i, j, using: self, other)
+            }
+        }
+        
+        return D[m - 1, n - 1]
+    }
+}
+
+private struct _LevenshteinMatrix {
+    let m, n: Int
+    
+    private var _values: [Int]
+    
+    init(m: Int, n: Int) {
+        self.m = m
+        self.n = n
+        
+        self._values = [Int](repeating: 0, count: m * n)
+    }
+    
+    subscript(i: Int, j: Int) -> Int {
+        get {
+            return _values[i + j * m]
+        }
+        set {
+            _values[i + j * m] = newValue
+        }
+    }
+    
+    func compute(at i: Int, _ j: Int, using u: String, _ v: String) -> Int {
+        let indexU = u.index(u.startIndex, offsetBy: i)
+        let indexV = v.index(v.startIndex, offsetBy: j)
+        
+        let a = u[indexU] == v[indexV] ? self[i - 1, j - 1] : Int.max
+        let b = self[i - 1, j - 1] + 1 // Replace
+        let c = self[i, j - 1] + 1 // Insert
+        let d = self[i - 1, j] + 1 // Delete
+        
+        return min(a, b, c, d)
+    }
+}
+
+
