b97939af7c638562df136414aa6c8ca53a5203fa
Cyrille Legrand <c.legrand@useradgents.com>
2024-01-29 12:13:39 +0100

    Add Privacy Manifest (mostly empty cause we don't track anything). Needs confirmation that it's correctly integrated. Method found on https://forums.developer.apple.com/forums/thread/742527

--- Package.swift
+++ Package.swift
@@ -12,8 +12,18 @@ let package = Package(
         .package(name: "RNCryptor", url: "https://github.com/RNCryptor/RNCryptor.git", from: "5.1.0")
     ],
     targets: [
-        .target(name: "Slab", dependencies: ["RNCryptor"]),
-        .testTarget(name: "SlabTests", dependencies: ["Slab"]),
+        .target(
+            name: "Slab",
+            dependencies: ["RNCryptor"],
+            resources: [
+                .copy("PrivacyInfo.xcprivacy")
+            ]
+        ),
+        .testTarget(
+            name: "SlabTests",
+            dependencies: ["Slab"]
+        ),
+        
     ],
     swiftLanguageVersions: [.v5]
 )
